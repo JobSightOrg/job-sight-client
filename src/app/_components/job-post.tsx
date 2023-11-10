3656ddc5f8428a3a8c0f7af632bb3a775527e5d1
Chris <chriswong90012@yahoo.com>
2023-11-10 00:12:48 -0800

    prisma addition

--- src/app/_components/job-post.tsx
+++ src/app/_components/job-post.tsx
@@ -1,23 +1,49 @@
+import { useEffect, useState } from "react";
+
+interface JobListings {
+  id: number;
+  url: string;
+  company: string;
+  createdAt: Date;
+  updatedAt?: Date;
+}
+
 export default function JobPost(): JSX.Element {
+  const [jobListings, setJobListings] = useState<JobListings[]>([]);
+
+  useEffect(() => {
+    fetch("/api/listing", {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => setJobListings(data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="mt-5">
-      <div className="select-none cursor-pointer border-2 border-black rounded-md flex flex-1 items-center p-4 hover:shadow">
-        <div className="flex-1 pl-1 mr-16">
-          <div className="font-medium">Company A</div>
-          <div className="text-gray-600 text-sm">
-            {" "}
-            <a
-              className="inline-block rounded-full text-white
+      {jobListings.map((jobListing) => (
+        <div
+          key={jobListing.id}
+          className="select-none cursor-pointer border-2 border-black rounded-md flex flex-1 items-center p-4 hover:shadow"
+        >
+          <div className="flex-1 pl-1 mr-16">
+            <div className="font-medium">{jobListing.company}</div>
+            <div className="text-gray-600 text-sm">
+              {" "}
+              <a
+                className="inline-block rounded-full text-white
                             bg-yellow-700
                             text-xs font-bold
                             mr-1 md:mr-2 mb-2 px-2 md:px-4 py-1 "
-              target="_blank"
-            >
-              COMPA
-            </a>{" "}
+                target="_blank"
+              >
+                COMPA
+              </a>{" "}
+            </div>
           </div>
         </div>
-      </div>
+      ))}
     </div>
   );
 }
---
+++ src/app/api/listing/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/db/prisma";
+
+export async function GET() {
+  try {
+    const jobListings = await prisma.jobListing?.findMany();
+    return NextResponse.json(jobListings ?? null);
+  } catch (err) {
+    console.error("Failed GET /api/listing\n", err);
+    return NextResponse.json({});
+  }
+}
