dcf0e649744a3bbcff5138a4a1b06a40b5744cf8
Chris <chriswong90012@yahoo.com>
2023-11-14 01:59:03 -0800

    prisma migrate

--- src/app/_components/add-modal.tsx
+++ src/app/_components/add-modal.tsx
@@ -1,10 +1,70 @@
 import Modal from "@/components/modal";
 import { Props } from "@/components/modal";
+import { useState } from "react";
+
+enum Company {
+  Google = "Google",
+  Meta = "Meta",
+  Amazon = "Amazon",
+  Apple = "Apple",
+}
 
 export default function AddModal({ isVisible, onClose }: Props): JSX.Element {
+  const [toggleDropdown, setToggleDropdown] = useState(false);
+
   return (
     <Modal isVisible={isVisible} title={"Add Job Listing"} onClose={onClose}>
       <div className="flex items-stretch mt-2">
+        <div className="relative inline-block text-left">
+          <div>
+            <button
+              type="button"
+              className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+              id="menu-button"
+              aria-expanded="true"
+              aria-haspopup="true"
+              onClick={() => setToggleDropdown(!toggleDropdown)}
+            >
+              Company
+              <svg
+                className="-mr-1 h-5 w-5 text-gray-400"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+          {toggleDropdown && (
+            <div
+              className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+              role="menu"
+              aria-orientation="vertical"
+              aria-labelledby="menu-button"
+              tabIndex={-1}
+            >
+              {(Object.keys(Company) as Array<keyof typeof Company>).map(
+                (key, idx) => (
+                  <div className="py-1" role="none" key={idx}>
+                    <button
+                      className="text-gray-700 block px-4 py-2 text-sm"
+                      role="menuitem"
+                      tabIndex={-1}
+                      id="menu-item-0"
+                    >
+                      {key}
+                    </button>
+                  </div>
+                )
+              )}
+            </div>
+          )}
+        </div>
         <div className="relative w-full">
           <div className="absolute text-gray-500 flex items-center px-2 border-r h-full">
             <svg
--- src/app/api/listing/route.ts
+++ src/app/api/listing/route.ts
@@ -4,9 +4,11 @@ import prisma from "@/db/prisma";
 export async function GET() {
   try {
     const jobListings = await prisma.jobListing?.findMany();
-    return NextResponse.json(jobListings ?? null);
+    return NextResponse.json(jobListings ?? []);
   } catch (err) {
     console.error("Failed GET /api/listing\n", err);
-    return NextResponse.json({});
+    return NextResponse.json([]);
   }
 }
+
+export async function POST() {}
