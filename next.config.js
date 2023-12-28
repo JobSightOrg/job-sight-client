050ba6202e0f3cc635ed0d985605bafbfc3a31e8
Chris <chriswong90012@yahoo.com>
2023-12-27 17:08:03 -0800

    user session

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["source.unsplash.com"],
+    domains: ["source.unsplash.com", "lh3.googleusercontent.com"],
   },
   webpack: (config, { isServer }) => {
     config.resolve.alias["@"] = path.resolve(__dirname);
--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,4 +1,4 @@
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import Logo from "~/public/logo.png";
 import DefaultPicture from "~/public/assets/icons/vercel.svg";
@@ -6,14 +6,23 @@ import DefaultPicture from "~/public/assets/icons/vercel.svg";
 export default function NavBar() {
   const { data: session } = useSession();
 
-  console.log(session);
-
   return (
     <nav className="flex justify-between items-center bg-gradient-to-r from-customLogoColor-500 via-customLogoColor-400 to-customLogoColor-300 w-full px-4 py-2">
       <Image className="w-[180px]" src={Logo} alt="Logo" priority={true} />
-      <div className="flex">
-        <Image src={session?.user?.image ?? DefaultPicture} alt="Profile" />
-        <p className="font-bold text-xl hover:underline hover:cursor-pointer">
+      <div className="flex items-center justify-between">
+        {session?.user && (
+          <Image
+            className="mr-5"
+            src={session.user?.image ?? DefaultPicture}
+            alt="Profile"
+            width={50}
+            height={50}
+          />
+        )}
+        <p
+          className="font-bold text-xl hover:underline hover:cursor-pointer"
+          onClick={() => signOut({ callbackUrl: "/auth/login" })}
+        >
           Logout
         </p>
       </div>
